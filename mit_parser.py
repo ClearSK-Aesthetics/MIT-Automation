8bfa7d52a79d093e5120786f223f9c306de84f01
ClearSK-Aesthetics <training@clearsk.com>
2026-02-21 17:07:04 +0800

    Enhance document processing functions in mit_parser
    Added functions to detect document nature and format dates. Updated MIT fields extraction logic.

--- mit_parser.py
+++ mit_parser.py
@@ -1,9 +1,25 @@
 import pytesseract
 from pdf2image import convert_from_path
 import re
+from datetime import datetime
 
 # change to your local path
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR"
+def detect_document_nature(text):
+    text_upper = text.upper()
+    if "SOA" in or"STATEMENT OF ACCOUNT" in text_upper:
+        return "SOA"
+    if "INVOICE" in text_upper:
+        return "PV-Payment"
+    if "RECEIPT" in text_upper:
+        return "PV-Receipt"
+    if "CREDIT NOTE" in text_upper:
+        return "PV-Credit Note"
+
+    return "PV-Payment" # default
+doc_nature = detect_document_nature(text
+        
+    
 
 
 def extract_text_from_scanned_pdf(pdf_path):
@@ -15,21 +31,30 @@ def extract_text_from_scanned_pdf(pdf_path):
         full_text += text + "\n"
 
     return full_text
-
-
+def format_date(date_string):
+    try:
+        date_obj = datetime.striptime(date_string.strip(),"%d/%m/%Y")
+        return date_onj.strftime("%d%m%Y")
+    except:
+        return ""
 def parse_mit_fields(text):
-    """
-    acocoridng to key words to extract MIT Columns
-    Based on MIT format to expand
-    """
-
-    vendor = re.search(r"(Supplier|Vendor)\s*:?\s*(.+)", text)
-    amount = re.search(r"(Total|Amount)\s*:?\s*\$?([\d,]+\.\d{2})", text)
-    date = re.search(r"(Date)\s*:?\s*(\d{2}/\d{2}/\d{4})", text)
+    invoice_no = re.search(r"(INV[-\d]+)", text)
+    amount = re.search(r"\$([\d,]+\.\d{2})", text)
+    date_match = re.search(r"(\d{2}/\d{2}/\d{4})", text)
+    formatted_date= format_date(raw_date)
+    vendor = re.search(r"([A-Z &\.]+ PTE\. LTD\.)", text)
 
     return {
-        "Vendor": vendor.group(2) if vendor else "Unknown",
-        "Amount": amount.group(2) if amount else "0",
-        "Date": date.group(2) if date else "Unknown",
-        "Raw_Text": text[:500]
+        "Receipt Date": date.group(1) if date else "",
+        "Date of Input": date.group(1) if date else "",
+        "Date of Document": date.group(1) if date else "",
+        "Sender Name": vendor.group(1) if vendor else "",
+        "Document Nature":"PV-Pay
+        "Description": text[:200],
+        "Invoice #": invoice_no.group(1) if invoice_no else "",
+        "Invoice Amount": amount.group(1) if amount else "",
+        "GST (if any)": "",
+        "Amount ($) Exc GST": "",
+        "PIC": "Auto-System",
+        "URL LINK":""
     }
